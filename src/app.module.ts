2df720a63d8d99768e4259623880a11475457128
AntonioVicente37 <githantonio@gmail.com>
2023-06-10 11:55:04 +0100

    Retirando os arquivos de erro

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    //UsuarioModule
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/usuario/usuario.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { UsuarioService } from './usuario.service';
-
-@Controller()
-export class UsuarioController {
-    constructor(private readonly usuarioService: UsuarioService) { }
-
- }
